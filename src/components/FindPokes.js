4ea13b6487cd3ed07592fe958683f51d4e4e9eec
GonzaloAgustin <gagustin.sk@gmail.com>
2021-10-05 14:06:49 -0300

    jsadodjaso

--- src/components/FindPokes.js
+++ src/components/FindPokes.js
@@ -8,7 +8,9 @@ export const FindPokes = () => {
 
     const [apiPok, setapiPok] = useState({
         name: null,
-        img: null
+        img: null,
+        loading: false,
+        mj: ''
     })
 
     const [searchTerm, setSearchTerm] = useState('')
@@ -20,19 +22,30 @@ export const FindPokes = () => {
     }
     
     useEffect(() => {
+            setapiPok({
+                name: null,
+                img: null,
+                loading: true,
+                mj: 'buscando..'})
             pokeFetch(pokes).then(
                 xd => 
                     {if( xd !== undefined)
                     {
-                            setapiPok({
-                                name: xd.name,
-                                img: xd.img
-                            })                  
-                    }else{
                         setapiPok({
-                            name: null,
-                            img: null
-                        })
+                            name: xd.name,
+                            img: xd.img,
+                            loading: false,
+                            mj: 'existe'
+                        })                  
+                    }else{
+                        setTimeout(() => {
+                            setapiPok({
+                                name: null,
+                                img: null,
+                                loading: false,
+                                mj: 'no existe'
+                            })
+                        }, 200);
                 }}
             )
     },[pokes])
@@ -46,18 +59,32 @@ export const FindPokes = () => {
     
     return (
         <>
-        <form onSubmit={ submitForm }>
+         <div class="row container center">
+        <form onSubmit={ submitForm } class="col s12 center-align"> 
+        <div class="input-field col s12 m4 l8"> 
         <input
         autoFocus
         type='text'
         autoComplete='off'
         className='live-search-field'
-        placeholder='Search here...'
+        placeholder='Find Pokémon'
         onChange={(e) => setSearchTerm(e.target.value)}
         />
+        </div>
         </form>
+
+
+        
+        <div class="center">
         <h1>{apiPok.name}</h1>
         < img src={apiPok.img} alt='' />
+        </div>
+
+        <h1>{apiPok.loading ? 'Buscando' : null}</h1>
+        <h1>{apiPok.mj === 'no existe' && apiPok.loading === false ? 'No existe' : null}</h1>
+        
+        </div>
         </>
     )
+    
 }
